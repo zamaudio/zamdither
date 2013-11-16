1dad665da2ed5d2bb83e822a3639270cbc43bbe1
Damien Zammit <damien@zamaudio.com>
2013-11-17 00:14:28 +1100

    Renamed file
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- zamdither.c
+++ zamdither.c
@@ -1,4 +1,4 @@
-/* zamdither.c  ZamClip mono soft clipper
+/* zamdither.c  ZamDither LV2 dither plugin
  * Copyright (C) 2013  Damien Zammit
  *
  * This program is free software; you can redistribute it and/or
